bfa5298c948d7a4c68f6541b77372788b163293f
valhayot <valeriehayot@gmail.com>
2021-07-04 09:02:03 -0400

    some refactoring

--- src/trx.cpp
+++ src/trx.cpp
@@ -5,15 +5,13 @@
 #include <json/json.h>
 using namespace std;
 
-int main(int argc, char *argv[])
+Json::Value load_header(zip_t *zfolder)
 {
-	cout << "test" << endl;
-	int *errorp;
-	char *path = strdup(argv[1]);
-	zip_t *zf = zip_open(path, 0, errorp);
-	zip_file_t *zh = zip_fopen(zf, "header.json", ZIP_FL_UNCHANGED);
+	// load file
+	zip_file_t *zh = zip_fopen(zfolder, "header.json", ZIP_FL_UNCHANGED);
 
-	int buff_len = 20 * sizeof(char);
+	// read data from file in chunks of 255 characters until data is fully loaded
+	int buff_len = 255 * sizeof(char);
 	char *buffer = (char *)malloc(buff_len);
 	char *jstream, *tmpstream;
 	jstream = NULL;
@@ -21,11 +19,10 @@ int main(int argc, char *argv[])
 	while ((nbytes = zip_fread(zh, buffer, buff_len - 1)) > 0)
 	{
 		buffer[buff_len - 1] = '\0';
-		//expand stream for another buffer of size 255
+		//expand stream for another buffer of size + 255
 
 		if (jstream != NULL)
 		{
-			// std::cout << strlen(jstream) << "-" << strlen(buffer) << "-" << nbytes << '\n';
 			tmpstream = (char *)malloc(strlen(jstream) + nbytes + 1);
 			strcpy(tmpstream, jstream);
 			strncat(tmpstream, buffer, nbytes);
@@ -33,15 +30,14 @@ int main(int argc, char *argv[])
 		}
 		else
 		{
-			// std::cout << "buff" << strlen(buffer) << "-" << nbytes << '\n';
 			tmpstream = strdup(buffer);
 		}
 
 		jstream = strndup(tmpstream, strlen(tmpstream));
-		// std::cout << strlen(jstream) << std::endl;
 		free(tmpstream);
 	}
-	cout << jstream << endl;
+
+	// convert jstream data into Json map.
 	Json::Value root;
 	JSONCPP_STRING err;
 	Json::CharReaderBuilder builder;
@@ -51,8 +47,24 @@ int main(int argc, char *argv[])
 					   &err))
 	{
 		std::cout << "error" << std::endl;
-		return EXIT_FAILURE;
+		return NULL;
 	}
 
-	std::cout << root["NB_VERTICES"].asString() << std::endl;
+	return root;
+}
+
+int main(int argc, char *argv[])
+{
+
+	// "Test cases" until i create more formal ones
+	int *errorp;
+	char *path = strdup(argv[1]);
+	zip_t *zf = zip_open(path, 0, errorp);
+	Json::Value root = load_header(zf);
+
+	std::cout << "**Reading header**" << std::endl;
+	std::cout << "Dimensions: " << root["DIMENSIONS"] << std::endl;
+	std::cout << "Number of streamlines: " << root["NB_STREAMLINES"] << std::endl;
+	std::cout << "Number of vertices: " << root["NB_VERTICES"] << std::endl;
+	std::cout << "Voxel to RASMM: " << root["VOXEL_TO_RASMM"] << std::endl;
 }
--- src/trx.h
+++ src/trx.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <zip.h>
+#include <json/json.h>
 #include <map>
 class TrxFile
 {
@@ -16,4 +17,13 @@ private:
 	int len();
 };
 
+/**
+ * This function loads the header json file
+ * stored within a Zip archive
+ * 
+ * @param[in] zfolder a pointer to an opened zip archive
+ * @param[out] header the JSONCpp root of the header. NULL on error.
+ *  
+ * */
+Json::Value load_header(zip_t *zfolder);
 std::ostream &operator<<(std::ostream &out, const TrxFile &TrxFile);
