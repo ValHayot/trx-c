0da450f65eb67e56a40cc25cea5b442ac8d15bbd
valhayot <valeriehayot@gmail.com>
2021-08-18 12:50:28 -0400

    some advancements with trx functions

--- src/trx.cpp
+++ src/trx.cpp
@@ -1,6 +1,9 @@
 #include "trx.h"
 #include <fstream>
 
+#include <errno.h>
+#define SYSERROR() errno
+
 //#define ZIP_DD_SIG 0x08074b50
 //#define ZIP_CD_SIG 0x06054b50
 using namespace Eigen;
@@ -43,29 +46,10 @@ Json::Value load_header(zip_t *zfolder)
 	zip_int64_t nbytes;
 	while ((nbytes = zip_fread(zh, buffer, buff_len - 1)) > 0)
 	{
-		//buffer[buff_len - 1] = '\0';
-
 		if (buffer != NULL)
 		{
 			jstream += string(buffer, nbytes);
 		}
-		//expand stream for another buffer of size + 255
-
-		// if (jstream.compare("") != 0)
-		// {
-		// 	//tmpstream = (char *)malloc(strlen(jstream) + nbytes + 1);
-		// 	//strcpy(tmpstream, jstream);
-		// 	//strncat(tmpstream, buffer, nbytes);
-		// 	//free(jstream);
-		// 	jstream += string(buffer);
-		// }
-		// else
-		// {
-		// 	tmpstream = strdup(buffer);
-		// }
-
-		// jstream = strndup(tmpstream, strlen(tmpstream));
-		// free(tmpstream);
 	}
 
 	// convert jstream data into Json map.
@@ -78,7 +62,7 @@ Json::Value load_header(zip_t *zfolder)
 					   &err))
 	{
 		std::cout << "error" << std::endl;
-		return NULL;
+		return 0;
 	}
 
 	return root;
@@ -89,20 +73,6 @@ int load_from_zip(const char *path)
 	int *errorp;
 	zip_t *zf = zip_open(path, 0, errorp);
 	Json::Value header = load_header(zf);
-
-	// no possibility to precise float32 apparently
-	// for (int x = 0; x < 4; x++)
-	// {
-	// 	for (int y = 0; y < 4; y++)
-	// 	{
-	// 		header["VOXEL_TO_RASMM"][x][y] = (header["VOXEL_TO_RASMM"][x][y]).asFloat();
-	// 	}
-	// }
-
-	// for (int i = 0; i < header["DIMENSIONS"].size(); i++)
-	// {
-	// 	header["DIMENSIONS"][i] = header["DIMENSIONS"][i].asUInt64();
-	// }
 
 	std::map<std::string, std::tuple<int, int>> file_pointer_size;
 	int global_pos = 0;
@@ -157,12 +127,179 @@ int load_from_zip(const char *path)
 	return 1;
 }
 
-TrxFile::TrxFile(int nb_vertices, int nb_streamlines, Json::Value init_as)
+void allocate_file(const std::string &path, const int size)
+{
+	std::ofstream file(path);
+	if (file.is_open())
+	{
+		std::string s(size, float(0));
+		file << s;
+		file.flush();
+		file.close();
+	}
+	else
+	{
+		std::cerr << "Failed to allocate file : " << SYSERROR() << std::endl;
+	}
+}
+
+//TODO: support FORTRAN ORDERING
+//template <typename Derived>
+mio::shared_mmap_sink _create_memmap(std::filesystem::path &filename, const MatrixXi &shape, std::string mode, std::string dtype, int offset)
+{
+	if (dtype.compare("bool") == 0)
+	{
+		std::filesystem::path ext = "bit";
+		filename.replace_extension(ext);
+	}
+
+	allocate_file(filename.string(), shape(0) * shape(1));
+	//std::error_code error;
+	mio::shared_mmap_sink rw_mmap(filename.string(), 0, mio::map_entire_file);
+
+	// should investigate further on creating mmap here rather than returning data pointer
+	return rw_mmap;
+}
+
+void TrxFile::_initialize_empty_trx(int nb_streamlines, int nb_vertices, TrxFile *init_as)
+{
+
+	//TODO: fix as tmpnam has issues with concurrency.
+	std::filesystem::path tmp_dir{std::filesystem::temp_directory_path() /= std::tmpnam(nullptr)};
+	std::filesystem::create_directory(tmp_dir);
+	std::cout << "Temporary folder for memmaps: " << tmp_dir << std::endl;
+
+	this->header["NB_VERTICES"] = nb_vertices;
+	this->header["NB_STREAMLINES"] = nb_streamlines;
+
+	if (init_as != NULL)
+	{
+		this->header["VOXEL_TO_RASMM"] = init_as->header["VOXEL_TO_RASMM"];
+		this->header["DIMENSIONS"] = init_as->header["DIMENSIONS"];
+	}
+
+	cout << "Initializing positions with dtype: "
+		 << "float16" << endl;
+	cout << "Initializing offsets with dtype: "
+		 << "uint16" << endl;
+	cout << "Initializing lengths with dtype: "
+		 << "uint32" << endl;
+
+	std::filesystem::path positions_filename;
+	positions_filename /= tmp_dir;
+	positions_filename /= "positions.3.float16";
+	Eigen::MatrixXi m(2, 1);
+	m << nb_vertices, 3;
+	//new (&trx.streamlines._data)
+	this->streamlines.mmap_pos = _create_memmap(positions_filename, m, std::string("w+"), std::string("float16"), 0);
+	new (&(this->streamlines._data)) Map<Matrix<half, Dynamic, Dynamic>>(reinterpret_cast<half *>(this->streamlines.mmap_pos.data()), m(0), m(1));
+
+	std::filesystem::path offsets_filename;
+	offsets_filename /= tmp_dir;
+	offsets_filename /= "offsets.uint16";
+	cout << "filesystem path " << offsets_filename << endl;
+	m << nb_streamlines, 1;
+	this->streamlines.mmap_off = _create_memmap(offsets_filename, m, std::string("w+"), std::string("uint16_t"), 0);
+	new (&(this->streamlines._offset)) Map<Matrix<uint16_t, Dynamic, 1>>(reinterpret_cast<uint16_t *>(this->streamlines.mmap_off.data()), m(0), m(1));
+
+	this->streamlines._lengths.resize(nb_streamlines, 0);
+
+	if (init_as != NULL)
+	{
+		if (init_as->data_per_vertex.size() > 0)
+		{
+			std::filesystem::path dpv_dirname;
+			dpv_dirname /= tmp_dir;
+			dpv_dirname /= "dpv";
+			std::filesystem::create_directory(dpv_dirname);
+		}
+		if (init_as->data_per_streamline.size() > 0)
+		{
+			std::filesystem::path dps_dirname;
+			dps_dirname /= tmp_dir;
+			dps_dirname /= "dps";
+			std::filesystem::create_directory(dps_dirname);
+		}
+
+		for (auto const &x : init_as->data_per_vertex)
+		{
+			int rows, cols;
+			Map<Matrix<half, Dynamic, Dynamic>> tmp_as = init_as->data_per_vertex[x.first]._data;
+			std::filesystem::path dpv_filename;
+			if (tmp_as.cols() == 1 || tmp_as.rows() == 1)
+			{
+				dpv_filename /= tmp_dir;
+				dpv_filename /= "dpv";
+				dpv_filename /= x.first + "." + "float16";
+				rows = nb_vertices;
+				cols = 1;
+			}
+			else
+			{
+				rows = nb_vertices;
+				cols = tmp_as.cols();
+
+				dpv_filename /= tmp_dir;
+				dpv_filename /= "dpv";
+				dpv_filename /= x.first + "." + std::to_string(cols) + "." + "float16";
+			}
+
+			cout << "Initializing " << x.first << " (dpv) with dtype: "
+				 << "float16" << endl;
+
+			Eigen::MatrixXi m(2, 1);
+			m << rows, cols;
+			this->data_per_vertex[x.first] = ArraySequence();
+			this->data_per_vertex[x.first].mmap_pos = _create_memmap(dpv_filename, m, std::string("w+"), std::string("float16"), 0);
+			new (&(this->data_per_vertex[x.first]._data)) Map<Matrix<uint16_t, Dynamic, Dynamic>>(reinterpret_cast<uint16_t *>(this->data_per_vertex[x.first].mmap_pos.data()), m(0), m(1));
+
+			this->data_per_vertex[x.first]._offset = this->streamlines._offset;
+			this->data_per_vertex[x.first]._lengths = this->streamlines._lengths;
+		}
+
+		for (auto const &x : init_as->data_per_streamline)
+		{
+			string dtype = "float16";
+			int rows, cols;
+			Map<Matrix<half, Dynamic, Dynamic>> tmp_as = init_as->data_per_streamline[x.first]._matrix;
+			std::filesystem::path dps_filename;
+
+			if (tmp_as.rows() == 1 || tmp_as.cols() == 1)
+			{
+				dps_filename /= tmp_dir;
+				dps_filename /= "dps";
+				dps_filename /= x.first + "." + dtype;
+				rows = nb_streamlines;
+			}
+			else
+			{
+				cols = tmp_as.cols();
+				rows = nb_streamlines;
+
+				dps_filename /= tmp_dir;
+				dps_filename /= "dps";
+				dps_filename /= x.first + "." + std::to_string(cols) + "." + dtype;
+			}
+
+			cout << "Initializing " << x.first << " (dps) with and dtype: " << dtype << endl;
+
+			Eigen::MatrixXi m(2, 1);
+			m << rows, cols;
+			this->data_per_streamline[x.first] = MMappedMatrix();
+			this->data_per_streamline[x.first].mmap = _create_memmap(dps_filename, m, std::string("w+"), dtype, 0);
+			new (&(this->data_per_streamline[x.first]._matrix)) Map<Matrix<half, Dynamic, Dynamic>>(reinterpret_cast<half *>(this->data_per_streamline[x.first].mmap.data()), m(0), m(1));
+		}
+	}
+
+	this->_uncompressed_folder_handle = tmp_dir;
+}
+
+TrxFile::TrxFile(int nb_vertices, int nb_streamlines, Json::Value init_as, std::string reference)
 {
 	MatrixXf affine(4, 4);
 	RowVectorXi dimensions(3);
 
-	if (init_as != NULL)
+	if (init_as != 0)
 	{
 		for (int i = 0; i < 4; i++)
 		{
@@ -187,7 +324,7 @@ TrxFile::TrxFile(int nb_vertices, int nb_streamlines, Json::Value init_as)
 
 	if (nb_vertices == 0 && nb_streamlines == 0)
 	{
-		if (init_as != NULL)
+		if (init_as != 0)
 		{
 			// raise error here
 			exit(1);
@@ -195,31 +332,138 @@ TrxFile::TrxFile(int nb_vertices, int nb_streamlines, Json::Value init_as)
 
 		// will remove as completely unecessary. using as placeholders
 		this->header = {};
-		this->streamlines = {};
-		this->lenghts = {};
-		this->offsets = {};
+		// this->streamlines = ArraySequence();
+		this->_uncompressed_folder_handle = "";
 
 		nb_vertices = 0;
 		nb_streamlines = 0;
 	}
-	//else if (nb_vertices != NULL && nb_streamlines != NULL)
+	else if (nb_vertices > 0 && nb_streamlines > 0)
+	{
+		std::cout << "Preallocating TrxFile with size " << nb_streamlines << " streamlines and " << nb_vertices << " vertices." << std::endl;
+		_initialize_empty_trx(nb_streamlines, nb_vertices);
+
+		//this->streamlines._data << half(1);
+		this->streamlines._offset << uint16_t(1);
+	}
 
 	this->header["VOXEL_TO_RASMM"] = affine;
 	this->header["DIMENSIONS"] = dimensions;
 	this->header["NB_VERTICES"] = nb_vertices;
 	this->header["NB_STREAMLINES"] = nb_streamlines;
-
-	//this->header["VOXEL_TO_RASMM"] = &affine;
-
-	//affine >> this->header["VOXEL_TO_RASMM"];
-	//dimensions >> this->header["DIMENSIONS"];
-	//nb_vertices >> this->header["NB_VERTICES"];
-	//nb_streamlines >> this->header["NB_STREAMLINES"];
 }
 
-TrxFile TrxFile::_create_trx_from_pointer(Json::Value header, std::map<std::string, std::tuple<int, int>> dict_pointer_size, std::string root_zip, std::string root)
+Dict assignHeader(Json::Value root)
 {
-	return TrxFile(0, 0);
+	Dict header;
+	MatrixXf affine(4, 4);
+	RowVectorXi dimensions(3);
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			affine << root["VOXEL_TO_RASMM"][i][j].asFloat();
+		}
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		dimensions[i] << root["DIMENSIONS"][i].asUInt();
+	}
+	header["VOXEL_TO_RASMM"] = affine;
+	header["DIMENSIONS"] = dimensions;
+	header["NB_VERTICES"] = (int)root["NB_VERTICES"].asUInt();
+	header["NB_STREAMLINES"] = (int)root["NB_STREAMLINES"].asUInt();
+
+	return header;
+}
+
+TrxFile *TrxFile::_create_trx_from_pointer(Json::Value header, std::map<std::string, std::tuple<int, int>> dict_pointer_size, std::string root_zip, std::string root)
+{
+	TrxFile *trx = new TrxFile();
+	trx->header = assignHeader(header);
+
+	// positions, offsets = None, None
+	Map<Matrix<half, Dynamic, Dynamic>> positions(NULL, 1, 1);
+	Map<Matrix<uint16_t, Dynamic, 1>> offsets(NULL, 1, 1);
+	std::filesystem::path filename;
+
+	for (auto const &x : dict_pointer_size)
+	{
+		std::filesystem::path elem_filename = x.first;
+		if (root_zip.size() == 0)
+		{
+			filename = root_zip;
+		}
+		else
+		{
+			filename = elem_filename;
+		}
+
+		std::filesystem::path folder = elem_filename.parent_path();
+
+		// _split_ext_with_dimensionality
+		std::string basename = elem_filename.filename().string();
+
+		std::string tokens[4];
+		int idx, prev_pos, curr_pos;
+		prev_pos = 0;
+		idx = 0;
+
+		while ((curr_pos = basename.find(".")) != std::string::npos)
+		{
+			tokens[idx] = basename.substr(prev_pos, curr_pos);
+			prev_pos = curr_pos + 1;
+			idx++;
+		}
+
+		if (idx < 2 || idx > 3)
+		{
+			throw("Invalid filename.");
+		}
+
+		basename = tokens[0];
+		std::string ext = "." + tokens[idx - 1];
+		int dim;
+
+		if (idx == 2)
+		{
+			dim = 1;
+		}
+		else
+		{
+			dim = std::stoi(tokens[1]);
+		}
+		_is_dtype_valid(ext);
+		// function completed
+
+		if (ext.compare(".bit") == 0)
+		{
+			ext = ".bool";
+		}
+	}
+
+	return trx;
+}
+
+void get_reference_info(std::string reference, const MatrixXf &affine, const RowVectorXi &dimensions)
+{
+	if (reference.find(".nii") != std::string::npos)
+	{
+	}
+	else if (reference.find(".trk") != std::string::npos)
+	{
+		// TODO: Create exception class
+		std::cout << "Trk reference not implemented" << std::endl;
+		std::exit(1);
+	}
+	else
+	{
+		// TODO: Create exception class
+		std::cout << "Trk reference not implemented" << std::endl;
+		std::exit(1);
+	}
 }
 
 int main(int argc, char *argv[])
@@ -237,5 +481,10 @@ int main(int argc, char *argv[])
 	std::cout << "Voxel to RASMM: " << header["VOXEL_TO_RASMM"] << std::endl;
 
 	load_from_zip(path);
+
+	TrxFile trx(1, 1);
+
+	std::cout << "Printing trk streamline data value " << trx.streamlines._data(0, 0) << endl;
+	std::cout << "Printing trk streamline offset value " << trx.streamlines._offset(0, 0) << endl;
 	return 1;
 }
--- src/trx.h
+++ src/trx.h
@@ -6,12 +6,38 @@
 #include <variant>
 #include <math.h>
 #include <Eigen/Core>
+#include <filesystem>
+
+#include <mio/mmap.hpp>
+#include <mio/shared_mmap.hpp>
 
 using namespace Eigen;
 
 const std::vector<std::string> dtypes({"int8", "int16", "int64", "uint8", "uint16", "uint32", "uint64", "float16", "float32", "float64"});
 
-typedef std::map<std::string, std::variant<int, MatrixXf, RowVectorXi>> Dict;
+typedef std::map<std::string, std::variant<int, MatrixXf, RowVectorXi, std::string, double>> Dict;
+
+struct ArraySequence
+{
+	// TODO: half precision for now. maybe fix or ask later.
+	Map<Matrix<half, Dynamic, Dynamic>> _data;
+	Map<Matrix<uint16_t, Dynamic, 1>> _offset;
+	std::vector<uint32_t> _lengths;
+	mio::shared_mmap_sink mmap_pos;
+	mio::shared_mmap_sink mmap_off;
+
+	ArraySequence() : _data(NULL, 1, 1), _offset(NULL, 1, 1){};
+	//Matrix<uint64_t, Dynamic, Dynamic> _offsets;
+	//Matrix<uint32_t, Dynamic, Dynamic> _lenghts;
+};
+
+struct MMappedMatrix
+{
+	Map<Matrix<half, Dynamic, Dynamic>> _matrix;
+	mio::shared_mmap_sink mmap;
+
+	MMappedMatrix() : _matrix(NULL, 1, 1){};
+};
 
 class TrxFile
 {
@@ -19,20 +45,35 @@ class TrxFile
 public:
 	// Data Members
 	Dict header;
-	std::vector<float> streamlines;
-	std::vector<uint64_t> offsets;
-	std::vector<uint32_t> lenghts;
+	ArraySequence streamlines;
+
+	Dict groups; //vector of strings as values
+
+	// int or float --check python float precision (singletons)
+	std::map<std::string, MMappedMatrix> data_per_streamline;
+	std::map<std::string, ArraySequence> data_per_vertex;
+	Dict data_per_group;
+	std::string _uncompressed_folder_handle;
 
 	// Member Functions()
 public:
 	//TrxFile(int nb_vertices = 0, int nb_streamlines = 0);
-	TrxFile(int nb_vertices = 0, int nb_streamlines = 0, Json::Value init_as = NULL);
+	TrxFile(int nb_vertices = 0, int nb_streamlines = 0, Json::Value init_as = 0, std::string reference = "");
 
-	static TrxFile _create_trx_from_pointer(Json::Value header, std::map<std::string, std::tuple<int, int>> dict_pointer_size, std::string root_zip = NULL, std::string root = NULL);
+	void _initialize_empty_trx(int nb_streamlines, int nb_vertices, TrxFile *init_as = NULL);
+	static TrxFile *_create_trx_from_pointer(Json::Value header, std::map<std::string, std::tuple<int, int>> dict_pointer_size, std::string root_zip = "", std::string root = "");
 
 private:
 	int len();
 };
+/**
+ * Converts Json header data to a Dict structure
+ * TODO: need to improve. Perhaps can just keep it in JSON format...
+ * 
+ * @param[in] root a Json::Value root obtained from reading a header file with JsonCPP
+ * @param[out] header a Dict header containing the same elements as the original root
+ * */
+Dict assignHeader(Json::Value root);
 
 /**
  * Determines data type size
@@ -71,4 +112,18 @@ Json::Value load_header(zip_t *zfolder);
  * 
  * */
 int load_from_zip(const char *path);
+
+/**
+ * Get affine and dimensions from a Nifti or Trk file (Adapted from dipy)
+ * 
+ * @param[in] reference a string pointing to a NIfTI or trk file to be used as reference
+ * @param[in] affine 4x4 affine matrix
+ * @param[in] dimensions vector of size 3
+ * 
+ * */
+void get_reference_info(std::string reference, const MatrixXf &affine, const RowVectorXf &dimensions);
+
+//template <typename Derived>
+mio::shared_mmap_sink _create_memmap(std::filesystem::path &filename, const MatrixXi &shape, std::string mode = "r", std::string dtype = "float32", int offset = 0);
+//void _create_memmap(std::filesystem::path &filename, std::string mode = "r", std::string dtype = "float32", int offset = 0);
 std::ostream &operator<<(std::ostream &out, const TrxFile &TrxFile);
